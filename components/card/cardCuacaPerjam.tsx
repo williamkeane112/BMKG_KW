c49a32a0d16a19b9cc55f6c4eccbf40870f1495b
williamkeane112 <williamkeanekeane@gmail.com>
2024-11-06 10:13:14 +0700

    New Repo

---
+++ components/card/cardCuacaPerjam.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cardCuacaPerjam = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default cardCuacaPerjam
---
+++ constants/Style.ts
@@ -0,0 +1,7 @@
+export const Style = {
+  container: {},
+  box: {},
+  statusBox: {},
+  
+};
+
